20112a9a418e8c88e22e7eaf4a3d5eb12ec74023
Austin <aokincaid@gmail.com>
2017-09-20 15:44:07 -0700

    Add test cases for basic expectations from BookSearch after books are processed

--- BookSearchTest.py
+++ BookSearchTest.py
@@ -4,14 +4,44 @@ from BookSearch import *
 class BookSearchTest(unittest.TestCase):
 
     test_json_path = 'json/bookdata.json'
+    test_book_search = BookSearch(test_json_path)
 
     def test_book_search_loading(self):
         """
         Test that the creation, and subsequent loading, of a new BookSearch with
         a provided path will contain books.
         """
-        test_book_search = BookSearch(self.test_json_path)
-        assert test_book_search._books is not None
+        # Expect the instantiated shared instance of BookSearch is not None.
+        assert self.test_book_search._books is not None
+
+        # Count the book keys available, should match the number provided in
+        # JSON. In this case, 9.
+        book_keys = self.test_book_search._books.keys()
+        assert len(book_keys) is 9
+
+    def test_book_search_find_books_with_sandwich(self):
+        """
+        Test that the BookSearch will find a common word, Sandwich.
+        """
+        query = 'sandwich'
+        found_books = self.test_book_search.find_books_with(query)
+        assert len(found_books) is 9
+
+    def test_book_search_find_books_with_oberons(self):
+        """
+        Test that the BookSearch will find an uncommon word that is often
+        written with punctuation, oberons (Oberon's as it usually appears in
+        text).
+        """
+        query = "Oberon's"
+        found_books = self.test_book_search.find_books_with(query)
+        assert len(found_books) is 2
+
+        # Now we try with the same word with out punctuation or an uppercase
+        # letter
+        query = "oberons"
+        found_books = self.test_book_search.find_books_with(query)
+        assert len(found_books) is 2
 
 if __name__ == '__main__':
     unittest.main()
